a31386f5f6b20c92a7d138c0a5e3f2564b46875b
Carlos Eduardo Hilario Ferreira <eduardohilarioferreira@gmail.com>
2024-10-24 21:27:02 -0300

    Tabela Categoria aceitando duplos valores e valores em lower

--- Arquivos/Scripts_SQL/SCRIPTS_TABELAS.sql
+++ Arquivos/Scripts_SQL/SCRIPTS_TABELAS.sql
@@ -16,10 +16,10 @@ CREATE TABLE FORNECEDOR(
     CONSTRAINT CHK_CNPJ_FORNECEDOR CHECK(REGEXP_LIKE(cnpj, '^\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}$')),
     CONSTRAINT CHK_CPF_FORNECEDOR CHECK(REGEXP_LIKE(cpf, '^\d{3}\.\d{3}\.\d{3}-\d{2}$'))
           
-)TBS_FORNECEDOR;
+) TABLESPACE TBS_FORNECEDOR;
 
 -- FILIAL
-CREATE TABLE FILIAL(
+CREATE TABLE FILIAL (
     id_filial               NUMBER(10) GENERATED ALWAYS AS IDENTITY CONSTRAINT PK_filial NOT NULL PRIMARY KEY,
     sku                     VARCHAR2(6) CONSTRAINT sku_filial NOT NULL UNIQUE,
     nome                    VARCHAR2(100) NOT NULL,
@@ -45,4 +45,24 @@ CREATE TABLE FILIAL(
     -- CHAVE ESTRANGEIRA REFERENCIANDO FORNECEDOR E A COLUNA (id_fornecedor_matriz)
     CONSTRAINT CHK_FK_FORNECEDOR FOREIGN KEY (id_fornecedor_matriz) REFERENCES FORNECEDOR (id_fornecedor)
           
-)TBS_FILIAL;
+) TABLESPACE TBS_FILIAL;
+
+
+-- TABELA CATEGORIA
+CREATE TABLE CATEGORIA (
+    id_categoria         NUMBER(10) GENERATED ALWAYS AS IDENTITY CONSTRAINT PK_CATEGORIA NOT NULL PRIMARY KEY,
+    nome                 VARCHAR2(100) NOT NULL UNIQUE CHECK (
+                                                              UPPER(nome) = 'RESFRIADOS' OR 
+                                                              UPPER(nome) = 'SALGADOS' OR 
+                                                              UPPER(nome) = 'CLIMATIZADOS' OR 
+                                                              UPPER(nome) = 'BOVINOS' OR 
+                                                              UPPER(nome) = 'SUINOS' OR 
+                                                              UPPER(nome) = 'CONGELADOS')
+ 
+    -- SESSÃO DE COMENTARIOS LEIA
+    
+                         /* Aqui provavelmente dará um erro de duplicação de informação porque o usuario pode colocar RESFRIADO sem o (S) e o banco irá aceitar
+                            futuramente vou arrumar com uma tiggers */
+                        
+                        /*Essa tabela vai dar um erro de violação do CHECK olhe o arquivo CDB1_USER_CARLOS que e compare com esse aqui.*/
+) TABLESPACE TBS_CATEGORIA;
